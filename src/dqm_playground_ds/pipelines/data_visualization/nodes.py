fe63b42bc6afcc4f1395c2dd174c429024d5adbf
Xavier Coubez <xavier.coubez@cern.ch>
2022-03-24 09:29:44 +0100

    Creating helper functions for plotting

--- src/dqm_playground_ds/pipelines/data_visualization/nodes.py
+++ src/dqm_playground_ds/pipelines/data_visualization/nodes.py
@@ -8,6 +8,13 @@ plt.style.use([hep.style.ROOT, hep.style.firamath])
 hep.rcParams.label.data = True
 hep.rcParams.label.paper = False
 
+def plot_one_time_serie(df, variable):
+    print(f"Plotting time serie for variable {variable}")
+    return
+
+def plot_one_correlation(df, variable1, variable2):
+    print(f"Plotting correlation between {variable1} and {variable2}")
+    return
 
 def plot_time_series(data: pd.DataFrame) -> None:
     """Plot time serie for all variables.
@@ -16,8 +23,10 @@ def plot_time_series(data: pd.DataFrame) -> None:
     Returns:
         None
     """
-    list_of_variables = data.columns.tolist()[3:]
+    list_of_variables = data.columns.tolist()[2:]
     print(list_of_variables)
+    for variable in list_of_variables:
+        plot_one_time_serie(data, variable)
     return None
 
 def plot_correlations(data: pd.DataFrame) -> None:
@@ -27,6 +36,8 @@ def plot_correlations(data: pd.DataFrame) -> None:
     Returns:
         None
     """
-    list_of_variables = data.columns.tolist()[3:]
+    list_of_variables = data.columns.tolist()[2:]
     print(list_of_variables)
+    for i in range(len(list_of_variables)-1):
+        plot_one_correlation(data, list_of_variables[i], list_of_variables[i+1])
     return None
