12c94a2aa371b7344c4b587109774aa6af321ab2
Xavier Coubez <xavier.coubez@cern.ch>
2022-03-24 12:39:39 +0100

    Creating all plots using MatplotlibWriter

--- src/dqm_playground_ds/pipelines/data_visualization/nodes.py
+++ src/dqm_playground_ds/pipelines/data_visualization/nodes.py
@@ -1,3 +1,5 @@
+from kedro.extras.datasets.matplotlib import MatplotlibWriter
+
 import pandas as pd
 
 import matplotlib.pyplot as plt
@@ -10,34 +12,59 @@ hep.rcParams.label.paper = False
 
 def plot_one_time_serie(df, variable):
     print(f"Plotting time serie for variable {variable}")
-    return
+
+    f, ax = plt.subplots()
+    hep.cms.label(loc=0)
+    plt.scatter(df['lumi'].tolist(), df[variable].tolist())
+    plt.xlabel('lumisection')
+    plt.ylabel(variable)
+    plt.tight_layout()
+    
+    return f
 
 def plot_one_correlation(df, variable1, variable2):
     print(f"Plotting correlation between {variable1} and {variable2}")
-    return
 
-def plot_time_series(data: pd.DataFrame) -> None:
+    f, ax = plt.subplots()
+    hep.cms.label(loc=0)
+    plt.scatter(df[variable1].tolist(), df[variable2].tolist())
+    plt.xlabel(variable1)
+    plt.ylabel(variable2)
+    plt.tight_layout()
+
+    return f
+
+def plot_time_series(data: pd.DataFrame) -> MatplotlibWriter:
     """Plot time serie for all variables.
     Args:
         data: aggregated dataframe
     Returns:
-        None
+        MatplotlibWriter creating all figures
     """
+
     list_of_variables = data.columns.tolist()[2:]
     print(list_of_variables)
-    for variable in list_of_variables:
-        plot_one_time_serie(data, variable)
-    return None
 
-def plot_correlations(data: pd.DataFrame) -> None:
+    list_of_figures = []
+    for variable in list_of_variables:
+        new_figure = plot_one_time_serie(data, variable)
+        list_of_figures.append(new_figure)
+
+    return list_of_figures
+
+def plot_correlations(data: pd.DataFrame) -> MatplotlibWriter:
     """Plot all pairwise correlations.
     Args:
         data: aggregated dataframe
     Returns:
-        None
+        MatplotlibWriter creating all figures
     """
     list_of_variables = data.columns.tolist()[2:]
     print(list_of_variables)
+
+    list_of_figures = []
     for i in range(len(list_of_variables)-1):
-        plot_one_correlation(data, list_of_variables[i], list_of_variables[i+1])
-    return None
+        new_figure = plot_one_correlation(data, list_of_variables[i], list_of_variables[i+1])
+        list_of_figures.append(new_figure)
+
+    return list_of_figures
