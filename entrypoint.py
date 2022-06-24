677a0fe4ac19ba00e85f744b46215a78e1e47481
Dimitris Papagiannis <nothingface0@gmail.com>
2022-06-24 12:13:55 +0200

    Logging, docstrings

--- entrypoint.py
+++ entrypoint.py
@@ -5,12 +5,26 @@ socket commands to start kedro pipelines
 import asyncio
 import logging
 
+logging.basicConfig(
+    level=logging.DEBUG,
+    style="{",
+    format="{levelname} - {asctime} - {module} - {message}",
+)
 logger = logging.getLogger(__name__)
 
+
+# Map command to subprocess to run
+# TODO: check how to run kedro programmatically instead of
+# running it as a subprocess
 CMD_TO_ACTION_MAP_SUBPROCESS = {"RUN_PIPELINE": "kedro run"}
 
 
 async def run(cmd):
+    """
+    Coroutine which runs a subprocess on the system
+
+    https://docs.python.org/3/library/asyncio-subprocess.html
+    """
     proc = await asyncio.create_subprocess_shell(
         cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
     )
@@ -19,20 +33,26 @@ async def run(cmd):
 
     print(f"[{cmd!r} exited with {proc.returncode}]")
     if stdout:
-        print(f"[stdout]\n{stdout.decode()}")
+        logger.info(f"[stdout]\n{stdout.decode()}")
     if stderr:
-        print(f"[stderr]\n{stderr.decode()}")
+        logger.error(f"[stderr]\n{stderr.decode()}")
 
 
-# Based on https://docs.python.org/3/library/asyncio-stream.html#tcp-echo-server-using-streams
-async def handle_echo(reader, writer):
+async def handle_client(reader, writer):
+    """
+    Coroutine which handles I/O for clients connected to socket server
+
+    https://docs.python.org/3/library/asyncio-stream.html#tcp-echo-server-using-streams
+    """
     command = None
+
+    # TODO: check message received
     while command != "QUIT":
         data = await reader.read(100)
         command = data.decode().strip()
         addr = writer.get_extra_info("peername")
 
-        print(f"Received {command!r} from {addr!r}")
+        logger.info(f"Received {command!r} from {addr!r}")
 
         # Check if requested action exists
         if command in CMD_TO_ACTION_MAP_SUBPROCESS:
@@ -45,15 +65,15 @@ async def handle_echo(reader, writer):
             writer.write(f"Requested action {command!r} not found\n".encode())
             await writer.drain()
 
-    print("Closed the connection")
+    logger.info("Closed the connection")
     writer.close()
 
 
 async def main():
-    server = await asyncio.start_server(handle_echo, "127.0.0.1", 8888)
+    server = await asyncio.start_server(handle_client, "127.0.0.1", 8888)
 
     addrs = ", ".join(str(sock.getsockname()) for sock in server.sockets)
-    print(f"Serving on {addrs}")
+    logger.info(f"Serving on {addrs}")
 
     async with server:
         await server.serve_forever()
